9a50a66f9a057f053e996e4c11b427f330d140e5
Tsubasa <scientistjishan@gmail.com>
2022-11-22 20:13:12 +0530

    Display value using enhanced for loop.

---
+++ Program_03.java
@@ -0,0 +1,13 @@
+/*
+ * Description : Display value of integer using enhanced for loop.
+ * Author      : Mr. Rabbit
+ */
+
+public class Program_03 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
